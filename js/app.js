5a441df9106c6ecf1ddce25a4ee5c4c4458c64f7
MohammadNasir520 <nasirahsan520@gmail.com>
2022-09-03 19:35:21 +0600

    spinner added

--- js/app.js
+++ js/app.js
@@ -10,23 +10,42 @@ const displayCatagories = (catagories) => {
     catagories.forEach(catagory => {
         const catagoryDiv = document.createElement('div');
         catagoryDiv.innerHTML = `
-        <p  onclick="newsLoad('${catagory.category_id}')">${catagory.category_name
+        <p id="catagory-name" onclick="newsLoad('${catagory.category_id}',toggleSpinner (true))">${catagory.category_name
             }</p>
         `
         catagoriesSection.appendChild(catagoryDiv)
         // console.log(catagory)
     })
-}
+};
+
 loadCatagories()
+
+// loader or spinner
+
+const toggleSpinner = (isLoading) => {
+    const spinnerConteiner = document.getElementById('spinner-container');
+    if (isLoading) {
+        spinnerConteiner.classList.remove('d-none')
+    }
+    else {
+        spinnerConteiner.classList.add('d-none')
+    }
+}
 
 const newsLoad = (id) => {
     fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
         .then(res => res.json())
         .then(data => displayNews(data.data))
+
 };
 
 
 const displayNews = (allNews) => {
+
+    if (allNews.length === 0) {
+
+    }
+
     const newsContainer = document.getElementById('news-container');
     newsContainer.textContent = ''
     allNews.forEach(news => {
@@ -53,14 +72,16 @@ const displayNews = (allNews) => {
                     <img style="height: 45px; width: 45px; border-radius: 50%; "
                         src="${news.author.img}" alt="">
                     <div class="ms-1">
-                        <h5>${news.author.name}</h5>
-                        <p>${news.author.published_date}</p>
+                        <h5>${news.author
+                .name ? news.author
+                .name : 'No name found'}</h5>
+                        <p>${news.author.published_date ? news.author.published_date : 'no date'}</p>
                     </div>
 
                 </div>
                 <div>
 
-                    <p><i class="fa-solid fa-eye"></i> ${news.total_view}</p>
+                    <p><i class="fa-solid fa-eye"></i> ${news.total_view ? news.total_view : 'no view'}</p>
                 </div>
 
                 <div>
@@ -81,10 +102,12 @@ const displayNews = (allNews) => {
        
 
         `
+        toggleSpinner(false)
         newsContainer.appendChild(newsDiv)
         // console.log(news)
     })
 };
+
 
 
 // news details for modal
@@ -94,22 +117,31 @@ const newsDetails = (newsId) => {
         .then(data => displayModal(data.data))
 };
 const displayModal = details => {
-
     const modalBody = document.getElementById('modal-body')
 
     details.forEach(detail => {
+
         modalBody.innerHTML = `
-        <img style="height: 45px; width: 45px; border-radius: 50%; src="${detail.author.img}"></img>
+        
         <p> author name: ${detail.author
                 .name ? detail.author
                 .name : 'No name found'}</p>
+
         <p> total view: ${detail.total_view
                 ? detail.total_view : 'No viewed yet'}</p>
+
         <p> Rating: ${detail.rating.number
 
                 ? detail.rating.number
                 : 'No rating yet'}</p>
 
+        <p> badge:  ${detail.rating.badge
+
+
+                ? detail.rating.badge
+
+                : 'No rating yet'}</p>
+                <p> published time: ${detail.author.published_date ? detail.author.published_date : 'no date and time  found'}</p>
         
         `
         console.log(detail)
@@ -117,4 +149,4 @@ const displayModal = details => {
 }
 newsDetails()
 
-// newsLoad();
+newsLoad();
